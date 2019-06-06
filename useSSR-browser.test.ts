0f6c32a0719522557ad722380c709209eba0ea27
Alex Cory <alexcory@fb.com>
2019-06-06 00:02:31 -0700

    fixing some tests

--- useSSR-browser.test.ts
+++ useSSR-browser.test.ts
@@ -2,9 +2,10 @@ import { renderHook } from 'react-hooks-testing-library'
 import useSSR from './useSSR';
 
 describe('useSSR', () => {
-  it('should correctly tell if we are on the browser', () => {
+  it('should correctly tell if we are in the browser', () => {
     const { result } = renderHook(() => useSSR())
-    const { isBrowser } = result.current
+    const { isBrowser, isServer } = result.current
     expect(isBrowser).toBe(true)
+    expect(isServer).toBe(false)
   });
 });
--- useSSR-server.test.ts
+++ useSSR-server.test.ts
@@ -2,9 +2,10 @@ import { renderHook } from 'react-hooks-testing-library'
 import useSSR from './useSSR';
 
 describe('useSSR', () => {
-  it('should show correctly tell if we are on the server', () => {
+  it('should correctly tell if we are on the server', () => {
     const { result } = renderHook(() => useSSR())
-    const { isServer } = result.current
+    const { isServer, isBrowser } = result.current
     expect(isServer).toBe(true)
+    expect(isBrowser).toBe(false)
   });
 });
