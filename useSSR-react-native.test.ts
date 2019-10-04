93a72b26a689178af5acaefc322f30c97e62b9f4
Alex Cory <results@alexcory.com>
2019-10-04 16:58:03 +0800

    adding support for react-native

--- useSSR-react-native.test.ts
+++ useSSR-react-native.test.ts
@@ -1,12 +1,14 @@
+import 'react-native'
 import { renderHook } from '@testing-library/react-hooks'
 import useSSR from './useSSR';
 
 describe('useSSR', () => {
-  it('should correctly tell if we are in the browser', () => {
+  // TODO: get this to work...
+  it('should correctly tell if we are in a react native environment', () => {
     const { result } = renderHook(() => useSSR())
     const { isBrowser, isServer, isNative } = result.current
-    expect(isBrowser).toBe(true)
+    expect(isBrowser).toBe(false)
     expect(isServer).toBe(false)
-    expect(isNative).toBe(false)
+    expect(isNative).toBe(true)
   });
 });
