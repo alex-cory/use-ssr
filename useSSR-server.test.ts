caa0558af56b2c4ca7037aea6d6b93b7ce1313d4
Alex Cory <results@alexcory.com>
2019-11-18 16:02:05 -0800

    updating

--- useSSR-server.test.ts
+++ useSSR-server.test.ts
@@ -4,9 +4,10 @@ import useSSR from './useSSR';
 describe('useSSR', () => {
   it('should correctly tell if we are on the server', () => {
     const { result } = renderHook(() => useSSR())
-    const { isServer, isBrowser, isNative } = result.current
+    const { isServer, isBrowser, isNative, device } = result.current
     expect(isServer).toBe(true)
     expect(isBrowser).toBe(false)
     expect(isNative).toBe(false)
+    expect(device).toBe('server')
   });
 });
